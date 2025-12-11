72d66ead7b1df87dec865b4df9f33e2ae120fadb
BARKA EBEN-EZEER <barkaebenezeer@gmail.com>
2025-12-11 08:32:42 +0100

    Second commit

--- Sample.c
+++ Sample.c
@@ -1,20 +1,39 @@
 #include "SampleLib.h"
+
 // Définition d'une fonction de calcul de factorielle.
-unsigned int fact( unsigned int value ) {
-unsigned int result = 1;
-while ( value > 1 ) {
-result *= value;
-value --;
+unsigned int fact(unsigned int value)
+{
+    unsigned int result = 1;
+    while (value > 1)
+    {
+        result *= value;
+        value--;
+    }
+    return result;
 }
-return result;
-}
+
 // Définition d'une fonction d'élévation à une puissance données.
-int power( int value, unsigned int pow ) {
-if ( pow == 0 ) return 1;
-if ( pow == 1 ) return value;int accumulator = 1;
-while( pow > 0 ) {
-accumulator *= value;
-pow--;
+int power(int value, unsigned int pow)
+{
+    if (pow == 0)
+        return 1;
+    if (pow == 1)
+        return value;
+    int accumulator = 1;
+    while (pow > 0)
+    {
+        accumulator *= value;
+        pow--;
+    }
+
+    return accumulator;
 }
-return accumulator;
+
+// Définition d'une fonction de permutation de deux nombres
+void permutation(int *a, int *b)
+{
+    int c = 0;
+    c = *a;
+    *a = *b;
+    *b = c;
 }
--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "SampleLib.h"
-int main( void ) {
-int result = power( 2, 3 );
-printf( "2³ == %d\n", result );
-result = fact( 6 );
-printf( "6! == %d\n", result );
-return EXIT_SUCCESS;
+
+int main(void)
+{
+    int result = power(2, 3);
+    printf("2³ == %d\n", result);
+
+    result = fact(6);
+    printf("6! == %d\n", result);
+
+    int first = 10;
+    int second = 20;
+    printf("first = %d # second = %d \n", first, second);
+    permutation(&first, &second);
+    printf("first = %d # second = %d", first, second);
+
+    return EXIT_SUCCESS;
 }
