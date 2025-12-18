4af6025c37cdff0ad3b32947f85c22eee96b766e
BARKA EBEN-EZEER <barkaebenezeer@gmail.com>
2025-12-18 23:45:10 +0100

    fifth commit

--- main.c
+++ main.c
@@ -4,53 +4,92 @@
 #include "bibliotheque.h"
 
 int main(void){
-    
-    int result = power(2, 3);
-    printf("2³ == %d\n", result);
 
+    int choix;
 
-    result = fact(6);
-    printf("6! == %d\n", result);
+    do {
+        // Affichage du menu
+        printf("\n===== MENU =====\n");
+        printf("1. Calculer une puissance\n");
+        printf("2. Calculer une factorielle\n");
+        printf("3. Permuter deux nombres\n");
+        printf("4. Resoudre une equation du second degre\n");
+        printf("5. Tester un nombre premier\n");
+        printf("6. Calculer la distance entre deux points\n");
+        printf("0. Quitter\n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
 
+        switch (choix) {
+            case 1: {
+                int base, exposant;
+                printf("Entrez la base et l'exposant : ");
+                scanf("%d %d", &base, &exposant);
+                printf("%d^%d = %d\n", base, exposant, power(base, exposant));
+                break;
+            }
+            case 2: {
+                int n;
+                printf("Entrez un entier : ");
+                scanf("%d", &n);
+                printf("%d! = %u\n", n, fact(n));
+                break;
+            }
+            case 3: {
+                int a, b;
+                printf("Entrez a : ");
+                scanf("%d", &a);
+                printf("Entrez b : ");
+                scanf("%d",&b);
+                permutation(&a, &b);
+                printf("Apres permutation : a = %d, b = %d\n", a, b);
+                break;
+            }
+            case 4: {
+                float a, b, c, x1, x2;
+                int nb;
+                printf("Entrez les coefficients a, b, c : ");
+                scanf("%f %f %f", &a, &b, &c);
+                nb = resolution_equation(a, b, c, &x1, &x2);
+                if (nb == -1)
+                    printf("Ce n'est pas une equation du second degre\n");
+                else if (nb == 0)
+                    printf("Pas de solution reelle\n");
+                else if (nb == 1)
+                    printf("Une solution : x = %f\n", x1);
+                else
+                    printf("Deux solutions : x1 = %f et x2 = %f\n", x1, x2);
+                break;
+            }
+            case 5: {
+                int n;
+                printf("Entrez un nombre : ");
+                scanf("%d", &n);
+                if (n < 0)
+                    printf("Le nombre est negatif\n");
+                else if (premier(n))
+                    printf("%d est premier\n", n);
+                else
+                    printf("%d n'est pas premier\n", n);
+                break;
+            }
+            case 6: {
+                Point A, B;
+                printf("Entrez les coordonnees du point A (x y) : ");
+                scanf("%f %f", &A.x, &A.y);
+                printf("Entrez les coordonnees du point B (x y) : ");
+                scanf("%f %f", &B.x, &B.y);
+                printf("La distance entre A et B est : %.2f\n", distance(A, B));
+                break;
+            }
+            case 0:
+                printf("Au revoir !\n");
+                break;
+            default:
+                printf("Choix invalide !\n");
+        }
 
-    int a = 10;
-    int b = 20;
-    printf("premier = %d et second = %d \n", a, b);
-    permutation(&a, &b);
-    printf("premier = %d et second = %d \n", a, b);
-    
-    float x1, x2;
-    int nb;
-    nb = resolution_equation(1, -3, 2, &x1, &x2);
-    if (nb==-1)
-        printf("Ce n'est pas une equation du second degre\n");
-    else if (nb== 0)
-        printf("Pas de solution reelle\n");
-    else if (nb==1)
-        printf("Une solution : x=%f\n", x1);
-    else
-        printf("Deux solutions : x1=%f et x2=%f\n", x1, x2);
-
-    
-    int valeur=4, nbre;
-    if (valeur<0)
-        printf("Le nombre que vous avez entré est négatif");
-    else{
-        nbre=premier(valeur);
-        if (nbre==0)
-            printf("Le nombre %d n'est pas premier\n",valeur);
-        else
-            printf("Le nombre %d est premier\n",valeur);
-    }
-
-
-    Point A, B;
-    printf("Entrez les coordonnees du point A (x y) : ");
-    scanf("%f %f", &A.x, &A.y);
-    printf("Entrez les coordonnees du point B (x y) : ");
-    scanf("%f %f", &B.x, &B.y);
-    float d = distance(A, B);
-    printf("La distance entre A et B est : %.2f\n", d);
+    } while (choix != 0);
 
     return EXIT_SUCCESS;
 }
